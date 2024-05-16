96347dc7fe8272460b1cf3d8c8ebb09ef96840b1
gleamrise <swk2460050897@outlook.com>
2024-05-16 21:25:37 +0800

    :core:database模块初始化

---
+++ core/database/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.li.android.library)
+    alias(libs.plugins.li.android.hilt)
+    alias(libs.plugins.li.android.room)
+}
+
+android {
+    namespace = "cn.li.core.database"
+}
+
+dependencies {
+
+}
